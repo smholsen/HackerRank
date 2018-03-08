56b256cd668b52f2b0aa27f497673503a001c32d
Simon <smholsen@stud.ntnu.no>
2018-03-08 16:59:40 +0100

    Added Save the Prisoner challenge

---
+++ Practice/Algorithms/Implementation/SaveThePrisoner/Solution.java
@@ -0,0 +1,23 @@
+package Practice.Algorithms.Implementation.SaveThePrisoner;
+
+import java.util.Scanner;
+
+public class Solution {
+
+    private static int saveThePrisoner(int n, int m, int s){
+        return (((s-1) + (m -1 % n)) % n) + 1;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int t = in.nextInt();
+        for(int a0 = 0; a0 < t; a0++){
+            int n = in.nextInt();
+            int m = in.nextInt();
+            int s = in.nextInt();
+            int result = saveThePrisoner(n, m, s);
+            System.out.println(result);
+        }
+    }
+
+}
